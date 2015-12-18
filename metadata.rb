2e5a9baa708161acefbefec82a623db861263521
pseudomuto <david.muto@gmail.com>
2015-12-18 16:49:23 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name "dev"
+maintainer "Sweeper Devs"
+maintainer_email "developers@sweeper.io"
+license "mit"
+description "Installs/Configures dev"
+long_description "Installs/Configures dev"
+version "0.1.0"
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: dev
+# Recipe:: default
+#
+# Copyright (c) 2015 Sweeper Devs, All Rights Reserved.
