0e8085b410e24740cc0eff8cb16e49b6c115ac22
pseudomuto <david.muto@gmail.com>
2015-12-19 13:11:08 -0500

    adding matchers that belong upstream to spec/support/matchers.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "chefspec"
 require "chefspec/berkshelf"
 
+Dir["./spec/support/**/*.rb"].each { |f| require f }
+
 RSpec.configure do |config|
   config.platform = "ubuntu"
   config.version  = "14.04"
